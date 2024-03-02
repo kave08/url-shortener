1adf678eed1883cb69233feb6c8df09ff528c6df
kave08 <kave.mailbox@gmail.com>
2024-03-02 12:13:45 +0330

    added redis

--- config/redis.go
+++ config/redis.go
@@ -1,1 +1,22 @@
 package config
+
+import (
+	"context"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func initializeRedisConnection() (*redis.Client, error) {
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     Cfg.Redis.Address,
+		Password: Cfg.Redis.Password,
+		DB:       0,
+	})
+
+	if err := rdb.Ping(context.Background()).Err(); err != nil {
+		return nil, err
+	}
+
+	return rdb, nil
+}
