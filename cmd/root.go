d241a3daad3057fd42eea58f390a418fd18a7717
kave08 <kave.mailbox@gmail.com>
2024-03-05 14:46:19 +0330

    added cobra to handle cli

--- cmd/root.go
+++ cmd/root.go
@@ -1,1 +1,27 @@
 package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var configPath string
+
+var rootCMD = &cobra.Command{
+	Use:   "url shortener",
+	Short: "url shortener service!",
+}
+
+func init() {
+	rootCMD.PersistentFlags().StringVarP(&configPath, "config-path", "c", "env.yaml", "path to config directory")
+
+}
+
+func Execute() {
+	if err := rootCMD.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
