7e886d037e54a007585ebc81d278945b93d1c8ca
kave08 <kave.mailbox@gmail.com>
2024-03-05 15:04:38 +0330

    added repository

--- repository/repository.go
+++ repository/repository.go
@@ -1,1 +1,22 @@
 package repository
+
+import (
+	"database/sql"
+	"github.com/kave08/url-shortener/repository/cache"
+	"github.com/kave08/url-shortener/repository/database"
+
+
+	"github.com/redis/go-redis/v9"
+)
+
+type Repository struct {
+	Database database.DatabaseInterface
+	Cache    cache.CacheInterface
+}
+
+func NewRepository(mdb *sql.DB, rdb *redis.Client) *Repository {
+	return &Repository{
+		Database: database.NewDatabase(mdb),
+		Cache:    cache.NewCache(rdb),
+	}
+}
