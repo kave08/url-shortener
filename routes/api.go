a2f83d02cc2f910444acc4caa2c55bbf0f3eb99a
kave08 <kave.mailbox@gmail.com>
2024-03-05 15:03:57 +0330

    added routes

--- routes/api.go
+++ routes/api.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	handlers "github.com/kave08/url-shortener/handler"
+	"github.com/labstack/echo/v4"
+)
+
+func InitializeGroup(e *echo.Echo, handler *handlers.Handlers) {
+	api := e.Group("/api/v1")
+
+	api.GET("/:shortUrl", handler.ResolvUrlHandler())
+	api.POST("/shorten", handler.ShortenUrlHandler())
+}
